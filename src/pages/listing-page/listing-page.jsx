dc24d11e40bb9a42f5e84eec93b03da441227d16
obafemi <oludahunsiobafemi18@gmail.com>
2022-04-05 15:37:06 +0100

    created the card component where each products will be viewed on the Product list page.

--- src/pages/listing-page/listing-page.jsx
+++ src/pages/listing-page/listing-page.jsx
@@ -3,6 +3,9 @@ import CartOverlay from "../../components/cart-overlay/cart-overlay";
 
 import { connect } from "react-redux";
 
+import { gql } from "@apollo/client";
+import { Query } from "@apollo/client/react/components";
+
 import styles from "./listing-page.module.css";
 import CurrencyDropdown from "../../components/currency-dropdown/currency-dropdown";
 
@@ -10,13 +13,50 @@ import Card from "../../components/card/card";
 
 class ListingPage extends React.Component {
   render() {
+    const PRODUCT_LISTS = gql`
+      {
+        categories {
+          name
+          products {
+            id
+            name
+            description
+            gallery
+            inStock
+            prices {
+              amount
+              currency {
+                label
+                symbol
+              }
+            }
+          }
+        }
+      }
+    `;
     const { hidden, currencyHidden } = this.props;
     return (
       <div className={styles.overview}>
         {hidden ? null : <CartOverlay />}
         {currencyHidden ? null : <CurrencyDropdown />}
         <div className={styles.title}> Category Name</div>
-        <Card />
+        <div className={styles["product-lists"]}>
+          {/* <Card /> */}
+          <Query query={PRODUCT_LISTS}>
+            {({ loading, error, data }) => {
+              if (loading) return <div>Loading...</div>;
+              if (error) return <div> Error! {error.message} </div>;
+              else {
+                const products = data.categories.map(
+                  (product) => product.products
+                );
+                // const product = products.map((product) => product.id);
+                // console.log(product);
+                console.log(products);
+              }
+            }}
+          </Query>
+        </div>
       </div>
     );
   }
--- src/queries/product-list/product-list.jsx
+++ src/queries/product-list/product-list.jsx
@@ -1,26 +1,3 @@
-import styles from "./product-list.module.css";
 
-import { gql, useQuery } from "@apollo/client";
 
-const PRODUCT_LIST = gql`
-  query {
-    categories {
-      name
-      products {
-        id
-        name
-      }
-    }
-  }
-`;
 
-const ProductList = () => {
-  const { loading, error, data } = useQuery(PRODUCT_LIST);
-  if (error) return <h3> Error : {error.message} </h3>;
-  if (loading) return <p>Loading</p>;
-  const response = data.categories.products.map((product) => product.id);
-  console.log(response);
-  return <div></div>;
-};
-
-export default ProductList;
