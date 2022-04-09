efa98b21c88563fdbe129fc2f05446515e9ef5b2
obafemi <oludahunsiobafemi18@gmail.com>
2022-04-09 11:54:06 +0100

    wrote the code for the functionality of adding and removing items in a Cart in the cart.util.js file.

--- src/redux/cart/cart.util.js
+++ src/redux/cart/cart.util.js
@@ -1,13 +1,29 @@
-// export const addItemToCart = (cartItems, cartItemToAdd) => {
-//     const existingItem = cartItems.find((cartItem) => cartItem.id === cartItemToAdd.id);
-//     if(existingItem) {
-//         //
-//     }
-// }
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+  if (existingItem) {
+    cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  } else {
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  }
+};
 
-// export const removeItemFromCart = (cartItems, cartItemToRemove) => {
-//     const existingItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
-//     if(existingItem) {
-
-//     }
-// }
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+  if (existingItem.quantity === 1) {
+    cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  } else {
+    cartItems.map((cartItem) =>
+      cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItemToRemove.quantity - 1 }
+        : cartItem
+    );
+  }
+};
