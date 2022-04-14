212ce12a48f3546073afe408ee1b0ecbeba941dc
obafemi <oludahunsiobafemi18@gmail.com>
2022-04-14 23:47:08 +0100

    imported the item count selector into the cart icon badge so that when items are added to cart, it indicated the numbers at the cart badge.

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -3,24 +3,31 @@ import { ReactComponent as Cart } from "../../assets/cart.svg";
 
 import { connect } from "react-redux";
 
+import { selectCartItemCount } from "../../redux/cart/cart.selector";
+
 import { toggleCart } from "../../redux/cart/cart.action";
 
 import styles from "./cart-icon.module.css";
+import { createStructuredSelector } from "reselect";
 
 class CartIcon extends React.Component {
   render() {
-    const { toggleCart } = this.props;
+    const { toggleCart, itemCount } = this.props;
     return (
       <div className={styles["cart-icon"]} onClick={toggleCart}>
         <Cart />
-        <div className={styles.badge}>0</div>
+        <div className={styles.badge}>{itemCount}</div>
       </div>
     );
   }
 }
 
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemCount,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   toggleCart: () => dispatch(toggleCart()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
