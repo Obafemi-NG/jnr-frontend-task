1860a4e80dd5ba7af615ee5aa76ecb3afec17dca
obafemi <oludahunsiobafemi18@gmail.com>
2022-04-16 21:57:32 +0100

    made the items added to cart be displayed in the mini cart.

--- src/components/cart-overlay/cart-overlay.jsx
+++ src/components/cart-overlay/cart-overlay.jsx
@@ -5,10 +5,15 @@ import { connect } from "react-redux";
 import { toggleCart } from "../../redux/cart/cart.action";
 
 import { withRouter } from "../../withRouter";
+import { createStructuredSelector } from "reselect";
+import { selectCartItems } from "../../redux/cart/cart.selector";
 
 class CartOverlay extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      imageIndex: 0,
+    };
     this.handleViewBag = this.handleViewBag.bind(this);
   }
 
@@ -17,10 +22,60 @@ class CartOverlay extends React.Component {
     this.props.toggleCart();
   };
 
+  handleViewNextImage = () => {
+    this.setState((prevState) => prevState.imageIndex + 1);
+  };
+
+  handleViewPrevImage = () => {
+    this.setState((prevState) => prevState.imageIndex - 1);
+  };
+
   render() {
+    const { cartItems } = this.props;
     return (
       <Modal>
-        <div className={styles["cart-items"]}></div>
+        {cartItems.length ? (
+          <div className={styles["cart-items-container"]}>
+            <div className={styles.title}>
+              {" "}
+              <span className={styles.headline}> My Bag, </span>{" "}
+              <span>
+                {" "}
+                {cartItems.length} {cartItems.length > 1 ? "Items" : "Item"}{" "}
+              </span>{" "}
+            </div>
+            {cartItems.map((cartItem) => {
+              return (
+                <div className={styles["cart-item"]}>
+                  <div className={styles["left-section"]}>
+                    <p> {cartItem.name} </p>
+                    <h4 className={styles.price}>
+                      {" "}
+                      {`${cartItem.symbol}${cartItem.amount} `}{" "}
+                    </h4>
+                  </div>
+                  <div className={styles["middle-section"]}>
+                    <div className={styles.increase}>+</div>
+                    <div className={styles.quantity}>{cartItem.quantity}</div>
+                    <div className={styles.decrease}>-</div>
+                  </div>
+                  <div className={styles["right-section"]}>
+                    <img
+                      className={styles["cart-image"]}
+                      src={cartItem.gallery[this.state.imageIndex]}
+                      alt={cartItem.name}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div className={styles["empty-cart-message"]}>
+            {" "}
+            Your Cart is Empty!{" "}
+          </div>
+        )}
         <div className={styles.cta}>
           <button
             onClick={this.handleViewBag}
@@ -36,7 +91,14 @@ class CartOverlay extends React.Component {
     );
   }
 }
+
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   toggleCart: () => dispatch(toggleCart()),
 });
-export default withRouter(connect(null, mapDispatchToProps)(CartOverlay));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(CartOverlay)
+);
